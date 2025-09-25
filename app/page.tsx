a20aaf8d44361b90a31238e4c356f1cea778bf56
Mufaqar <mufaqar@gmail.com>
2025-09-25 20:44:56 +0500

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,19 @@
 //import Logo from "@/components/home/logo";
 //import TrendingProducts from "@/components/home/product";
 
+import Hero from "@/components/home/Hero";
+import TrendingProducts from "@/components/home/TrendingProducts";
+
 
 export default function Home() {
   return (
     <main>
-      {/* <Hero />
+      <Hero />
       <section>
         <TrendingProducts />
-        <Logo />
-      </section> */}
-      123
+        {/* <Logo /> */}
+      </section>
+    
     </main>
   );
 }
